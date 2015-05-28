05aa21fa80affdefa16cdcd914a4be2e69e236e3
neias <emreacara@gmail.com>
2015-05-28 18:27:18 +0300

    menu eklendi ve bootstap uyumu yapıldı #fixes 1

--- controller/menu.php
+++ controller/menu.php
@@ -1,9 +1,26 @@
 <?php
 class ControllerMenu extends Controller{
     public function index(){
-        $this->load->model('menu');
+        $this->load->library('menu');
 
-        $this->data['main_menu'] = $this->model_menu->mainMenu();
+        // $this->load->model('menu');
+
+        // $this->model_menu->mainMenu();
+
+        $menu = menu::factory()
+              ->add('About Us', '/about-us/', menu::factory()
+                    ->add('Who We Are', '/who-we-are/')
+                    ->add('What We Do', '/what-we-do/')
+                    ->add('Other Things', '/other-things/'))
+              ->add('Random', '/random/', menu::factory()
+                    ->add('Link One', '/link-one/')
+                    ->add('Link Two', '/link-two/', menu::factory()
+                          ->add('Level Three', '/level-three/')));
+
+        $attrs = array('class' => 'nav navbar-nav');
+        $active = $_SERVER['REQUEST_URI'];
+
+        $this->data['main_menu'] = $menu->render($attrs, $active);
 
         $this->render('menu');
     }
--- model/menu.php
+++ model/menu.php
@@ -1,7 +1,35 @@
 <?php
 class ModelMenu extends Model{
     public function mainMenu(){
-        $menu = array('emre', 'acar');
+        $menu = array
+              (
+                  array
+                  (
+                      'title' => 'Item One',
+                      'url' => '#item_one',
+                  ),
+                  array
+                  (
+                      'title' => 'Item Two',
+                      'url' => '#item_two',
+                      'children' => array
+                      (
+                          array
+                          (
+                              'title' => 'Item Three',
+                              'url' => '#item_three',
+                          ),
+                          array
+                          (
+                              'title' => 'Item Four',
+                              'url' => '#item_four',
+                          )
+                      )
+                  )
+              );
+
+
+
         return $menu;
     }
 }
